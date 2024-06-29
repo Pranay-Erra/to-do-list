7a949f28e682760fd76503e53b69ba6b69e7743d
[Pranay-Erra] <[pranayerra2003@gmail.com]>
2024-06-29 12:33:21 +0530

    added strike off

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,19 +6,18 @@ const Todo = (props) => {
 
     return (
         <ul className='todo'>
-            {tasks
-                .map((task, index) =>
-                    <li key={index}>
-                        <div className='checkAndTask'>
-                            <label className='checkContainer'>
-                                <input type="checkbox" onClick={() => handleComplete(index)} />
-                                <span className="checkmark"></span>
-                            </label>
-                            <span>{task.task}</span>
-                        </div>
-                        <button onClick={() => handleRemove(index)}><i className="fa-solid fa-trash-can"></i></button>
-                    </li>
-                )}
+            {tasks.map((task, index) =>
+                <li key={index}>
+                    <div className='checkAndTask'>
+                        <label className='checkContainer'>
+                            <input type="checkbox" checked={task.completed} onChange={() => handleComplete(index)} />
+                            <span className="checkmark"></span>
+                        </label>
+                        <span className={task.completed ? 'completed' : ''}>{task.task}</span>
+                    </div>
+                    <button onClick={() => handleRemove(index)}><i className="fa-solid fa-trash-can"></i></button>
+                </li>
+            )}
             {tasks.length > 1 && <p><button className='deleteAll' onClick={() => handleRemoveAll()}><i className="fa-solid fa-eraser"></i>Delete all</button></p>}
         </ul>
     );
